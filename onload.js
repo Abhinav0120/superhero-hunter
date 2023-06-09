bbce967186fc9e2a1fb810663de9ef5d3097f4ad
Abhinav <ajagekarajagekar@gmail.com>
2023-06-09 05:53:17 +0530

    Displayed SuperHeros in super hero gallery and also completed discription section

---
+++ onload.js
@@ -0,0 +1,65 @@
+(() => { 
+    superHeroGallery = document.getElementById('gallery-section');
+
+    const ts = '1686007360659';
+    const publicKey = '0bcbedd6b749ca1b9f3a3cbd4d3118c4';
+    const hashVal = '410f54114af2ae847e0c490c47c9f1db';
+
+    function onload(){
+        const timeStamp = ts;
+        const apiKey = publicKey; 
+        const hashValue = hashVal;
+        const limit = 40;
+        const url = `https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${hashValue}&limit=${limit}`;
+    
+        fetch(url)
+        .then((response)=>{
+            if(!response.ok){
+                throw new Error('Network response was not ok');
+            }
+
+            return response.json();
+        })
+        .then((data)=>{
+            if(data.data.results.length===0){
+                console.error('Characters not found');
+                return;
+            }
+
+            console.log(data);
+
+            const superheroes = data.data.results;
+
+            superheroes.forEach((superhero)=>{
+                // Create a contianer for each superhero
+                const superheroContainer = document.createElement('div');
+                superheroContainer.classList.add('superhero-container');
+
+                // Create an image element for the superheros's thumbnail
+                const thumbnailUrl = `${superhero.thumbnail.path}.${superhero.thumbnail.extension}`;
+                const thubmnailImage = document.createElement('img');
+                thubmnailImage.src = thumbnailUrl;
+                thubmnailImage.classList.add('superhero-Thumbnail');
+
+                // create a heading element for the superhero's name
+                const nameHeading = document.createElement('h3');
+                nameHeading.classList.add('suprehero-name');
+                nameHeading.textContent = superhero.name;
+
+                //append thubmnailImage and name to superheroContainer
+                superheroContainer.appendChild(thubmnailImage);
+                superheroContainer.appendChild(nameHeading);
+
+                // append the superherocontainer to the superHeroGallery
+                superHeroGallery.appendChild(superheroContainer);
+                
+            })
+        })
+        .catch((error)=>{
+            console.error(error);
+        })
+    }
+
+    onload();
+
+})();
--- script.js
+++ script.js
@@ -63,6 +63,65 @@ function findSuperHero(){
             superheroImgContainer.innerHTML = ` 
                 <img id="superhero-img" src="${thumbnailUrl}" alt="Super hero">
             `
+
+            const descriptionContainer = document.getElementById('description-container');
+            // const description = document.getElementById('description');
+            let characterDescription = characterData.data.results[0].description;
+            if(!characterDescription){
+                characterDescription = "Information is Classified";
+            }
+            const characterName = characterData.data.results[0].name;
+            descriptionContainer.innerHTML = `
+                <h3>
+                    ${characterName}
+                </h3>
+                <p id="description">
+                    ${characterDescription}
+                </p>
+            `
+
+            const comicUrl = `https://gateway.marvel.com/v1/public/characters/${characterId}/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${hashValue}`;
+            fetch(comicUrl)
+            .then((response)=>{
+                if(!response.ok){
+                    throw new Error('Network response was not Ok');
+                }
+
+                return response.json();
+            })
+            .then((comicsData)=>{
+                if(comicsData.data.results.length === 0){
+                    console.error("Comic not found");
+                    return;
+                }
+                console.log(comicsData);
+
+                const comics = comicsData.data.results;
+                const comicsContainer = document.getElementById('comics-container');
+                comicsContainer.innerHTML = '';
+
+                comics.forEach((comic)=>{
+                   
+                    const comicWrapper = document.createElement('div');
+                    comicWrapper.classList.add('comic');
+                    const imgSrc = `${comic.thumbnail.path}.${comic.thumbnail.extension}`;
+                    const comicTitle = `${comic.title}`;
+                    comicWrapper.innerHTML = `
+                        <div class="img-container">
+                            <img src="${imgSrc}" alt="comic">
+                        </div>
+                        <span class="name">
+                           ${comicTitle}
+                        </span>
+                    `
+                    comicsContainer.appendChild(comicWrapper);
+                });
+
+            })
+            .catch((error)=>{
+                console.error(error);
+            })
+
         })
         .catch((error)=>{
             console.error(error);
